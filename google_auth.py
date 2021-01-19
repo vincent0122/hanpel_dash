a0d359798f20cec3121f5c13d98267cbbd2065c5
vinc <>
2021-01-20 07:01:32 +0900

    del venv

--- google_auth.py
+++ google_auth.py
@@ -6,6 +6,6 @@ def get_google():
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'google-credentials.json', scope)  # Your json file here
+        './creden/google-credentials.json', scope)  # Your json file here
     gc = gspread.authorize(credentials)
     return(gc)
